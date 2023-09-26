3933d3bcd67933d13da0621af22bbb2c17b3555d
Tóth János <h985829@stud.u-szeged.hu>
2023-09-26 11:23:38 +0200

    feat: Trending articles

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ const URL = 'https://api.slingacademy.com/v1/sample-data/photos/';
 const images = {};
 const imagesMaxCount = 5;
 const articleMaxCount = 9;
+const trendingArticlesMaxCount = 5;
 let articles = [];
 let currentIndex = 0;
 let selectedElement = document.getElementById("selected-element");
 let selectedFilter = document.getElementById("selected-filter");
+let mainTrendingArticle = document.getElementById("main-trending");
 
 const fetchData = async (url, imageId) => {
     try {
@@ -75,19 +77,17 @@ async function createArticles(){
         let article = {
             id: i+1,
             date: "29.Jun.2019",
-            title: data.photo.title
+            title: data.photo.title,
+            url: `url('${data.photo.url}')`
         };
-        articles.push(article);
+        articles[i] = article;
 
-        const container = document.createElement("div");
-        container.classList.add("article-container");
-
-        const content = document.createElement("div");
-        content.classList.add("article-content");
+        const div = document.createElement("div");
+        div.classList.add("article-container");
 
         const image = document.createElement("div");
         image.classList.add("article-image");
-        image.style.backgroundImage = `url('${data.photo.url}')`;
+        image.style.backgroundImage = article.url;
 
         const date = document.createElement("div");
         date.classList.add("article-date");
@@ -95,19 +95,68 @@ async function createArticles(){
 
         const title = document.createElement("div");
         title.classList.add("article-title");
-        title.innerHTML = data.photo.title;
+        title.innerHTML = article.title;
 
-        content.appendChild(image);
-        content.appendChild(date);
-        content.appendChild(title);
-        container.appendChild(content);
+        div.appendChild(image);
+        div.appendChild(date);
+        div.appendChild(title);
+        articleContainer.appendChild(div);
+
+    }
+}
+
+async function creaeteTrenidnArticles() {
+    let articleContainer = document.getElementById("articles-trending")
+    for(let i = 0; i < trendingArticlesMaxCount; i++){
+        let article;
+        if(i < articles.length) {
+            article = articles[i];
+        }else{
+            let data = await fetchData(URL, i+1);
+            article = {
+                id: i+1,
+                date: "29.Jun.2019",
+                title: data.photo.title,
+                url: `url('${data.photo.url}')`
+            };
+            articles.push(article);
+        }
+
+        console.log(articles[i].id)
+
+        const container = document.createElement("div");
+        container.classList.add("article-container");
+
+        const image = document.createElement("div");
+        image.classList.add("trending-image");
+        if(!mainTrendingArticle) {
+            image.id = "main-trending";
+            mainTrendingArticle = image;
+        }
+        image.style.backgroundImage = article.url;
+
+        const date = document.createElement("div");
+        date.classList.add("trending-date");
+        date.innerHTML = "29.June.2019";
+
+        const title = document.createElement("div");
+        title.classList.add("trending-title");
+        title.innerHTML = article.title;
+
+        image.appendChild(date);
+        image.appendChild(title);
+        container.appendChild(image);
         articleContainer.appendChild(container);
 
-        console.log(i)
+    }
+    for(let i = 0; i < articles.length; i++){
+        console.log(articles[i]);
     }
 }
 
 createArticles();
 
+
+creaeteTrenidnArticles();
 showImage();
 
